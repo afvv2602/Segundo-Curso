0e56745a76aaa7b3f8df7f6db858bfe371405c5b
afvv2602 <44574101+afvv2602@users.noreply.github.com>
2023-04-25 13:12:42 +0200

    Galeria terminada

--- Client side/Js/Galeria/js/scripts.js
+++ Client side/Js/Galeria/js/scripts.js
@@ -3,25 +3,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const galeria = document.getElementById('galeria');
     const formulario = document.getElementById('formulario');
 
+    // Guardamos las referencias de las imagenes
     const obras = {
         exposicion1: [
             { src: 'imagenes/exposicion1/obra1.jpg', nombre: 'Obra 1' },
             { src: 'imagenes/exposicion1/obra2.jpg', nombre: 'Obra 2' },
+            { src: 'imagenes/exposicion1/obra3.jpg', nombre: 'Obra 3' },
+            { src: 'imagenes/exposicion1/obra4.jpg', nombre: 'Obra 4' },
+            { src: 'imagenes/exposicion1/obra5.jpg', nombre: 'Obra 5' },
         ],
         exposicion2: [
-            { src: 'imagenes/exposicion2/obra1.jpg', nombre: 'Obra 1' },
+            { src: 'imagenes/exposicion2/obra1.jpg', nombre: 'Obra1' },
             { src: 'imagenes/exposicion2/obra2.jpg', nombre: 'Obra 2' },
-        ],
-        exposicion3: [
+            { src: 'imagenes/exposicion2/obra3.jpg', nombre: 'Obra 3' },
+            { src: 'imagenes/exposicion2/obra4.jpg', nombre: 'Obra 4' },
+            { src: 'imagenes/exposicion2/obra5.jpg', nombre: 'Obra 5' },
+
+            ],
+            exposicion3: [
             { src: 'imagenes/exposicion3/obra1.jpg', nombre: 'Obra 1' },
             { src: 'imagenes/exposicion3/obra2.jpg', nombre: 'Obra 2' },
-        ],
-    };
+            { src: 'imagenes/exposicion3/obra3.jpg', nombre: 'Obra 3' },
+            { src: 'imagenes/exposicion3/obra4.jpg', nombre: 'Obra 4' },
+            { src: 'imagenes/exposicion3/obra5.jpg', nombre: 'Obra 5' },
 
-    exposicionesSelect.addEventListener('change', () => {
+         ],
+     };
+
+     // Funcion de creacion del carrousel
+     exposicionesSelect.addEventListener('change', () => {
         const exposicion = obras[exposicionesSelect.value];
         galeria.innerHTML = '';
-
+    
+        // Crear carrusel con la clase carrusel
+        const carrusel = document.createElement('div');
+        carrusel.classList.add('carrusel');
+        
+        //Por cada obra se crea una imagen
         exposicion.forEach(obra => {
             const imagen = document.createElement('figure');
             imagen.classList.add('galeria-imagen');
@@ -29,16 +47,36 @@ document.addEventListener('DOMContentLoaded', () => {
                 <img src="${obra.src}" alt="${obra.nombre}">
                 <figcaption>${obra.nombre}</figcaption>
             `;
-            galeria.appendChild(imagen);
+            carrusel.appendChild(imagen);
+            
+            //Cada imagen se le pone un listener para que se pueda crear un popup para cada imagen
+            imagen.addEventListener('click', () => {
+                const popUp = crearPopUp(obra.src);
+                document.body.appendChild(popUp);
+                popUp.style.display = 'flex';
+            });
         });
+    
+        galeria.appendChild(carrusel);
     });
-
+    
+    // Funcion para crear el popup cuando se selecciona una imagen
+    function crearPopUp(src) {
+        const popUp = document.createElement('div');
+        popUp.classList.add('popup');
+        popUp.innerHTML = `
+            <img class="popup-img" src="${src}" alt="">
+        `;
+        popUp.addEventListener('click', () => {
+            popUp.remove();
+        });
+    
+        return popUp;
+    }
+    
+    //Se envia a otra pagina cuando se pulsa el boton
     formulario.addEventListener('submit', (e) => {
         e.preventDefault();
-        const nombre = formulario.nombre.value;
-        const exposicionFavorita = formulario.exposicionFavorita.value;
-        const enlace = `${nombre}-${exposicionFavorita}-entradas.pdf`;
-
-        alert(`Gracias por tu compra, ${nombre}! Descarga tus entradas aquí: ${enlace}`);
+        window.location.href = "compra.html";
     });
 });
