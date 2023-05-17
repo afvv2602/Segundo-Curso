85fed1859774172dae592584fc443b26315c1c69
Adrian Fernandez <afvv26002@gmail.com>
2023-05-17 10:59:37 +0200

    Checkout

--- Web/Client side/Js/Formulario/script.js
+++ Web/Client side/Js/Formulario/script.js
@@ -1,64 +1,51 @@
-function validacion() {
-    if (validaTexto(document.getElementById("apellido1").value ) ) {
-        var error = document.getElementById("errorapellido1");
-        error.parentNode.removeChild(error);
-        var label = document.getElementById("apellido1").value;
-        var resultado = document.getElementById("apellido1f");
-        resultado.value = label;
-    }
-    if (validaTexto(document.getElementById("apellido2").value )) {
-        var error = document.getElementById("errorapellido2");
-        error.parentNode.removeChild(error);
-        var label= document.getElementById("apellido2").value;
-        var resultado = document.getElementById("apellido2f");
-        resultado.value = label;
-    }
-    if (validaTexto(document.getElementById("nombre").value )){
-        var error = document.getElementById("errornombre");
-        error.parentNode.removeChild(error);
-        var label = document.getElementById("nombre").value;
-        var resultado = document.getElementById("nombref");
-        resultado.value = label;
-    }
-    if (validaNum(document.getElementById("nota").value )){
-        var error = document.getElementById("errornota");
-        error.parentNode.removeChild(error);
-        var label = document.getElementById("nota").value;
-        var resultado = document.getElementById("notaf");
-        resultado.value = label;
-    }
-    if (validaNum(document.getElementById("edad").value )){
-        var error = document.getElementById("erroredad");
-        error.parentNode.removeChild(error);
-        var label = document.getElementById("edad").value;
-        var resultado = document.getElementById("edadf");
-        resultado.value = label;
-    }
-    if(validaLista(document.getElementById("valoracion").selectedIndex)){
-        var error = document.getElementById("errorvaloracion");
-        error.parentNode.removeChild(error);
-        var label = document.getElementById("valoracion").value;
-        var resultado = document.getElementById("valoracionf");
-        resultado.value = label;
-    }
-}
+document.addEventListener('DOMContentLoaded', (event) => {
 
+    document.getElementById("validarNombre").addEventListener("click", validarNombre);
+    document.getElementById("validarNota").addEventListener("click", validarNota);
+    document.getElementById("validarValoracion").addEventListener("click", validarValoracion);
+    document.getElementById("validarEdad").addEventListener("click", validarEdad);
 
-function validaTexto(valor){
-if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-    return false;
-  }
-  else return true;
-  } 
-function validaNum(valor){
-    if( isNaN(valor) ) {
-        return false;
-      }else return true;
-}
+    function validarNombre() {
+        var nombre = document.getElementById("nombre").value;
+        var apellido1 = document.getElementById("apellido1").value;
+        var apellido2 = document.getElementById("apellido2").value;
+        document.getElementById("nombreCompleto").textContent = nombre + " " + apellido1 + " " + apellido2;
+    }
 
+    function validarNota() {
+        var nota = document.getElementById("nota").value;
+        var notaTexto;
+        if (nota < 5) {
+            notaTexto = "Insuficiente";
+        } else if (nota < 6) {
+            notaTexto = "Suficiente";
+        } else if (nota < 7) {
+            notaTexto = "Bien";
+        } else if (nota < 9) {
+            notaTexto = "Notable";
+        } else {
+            notaTexto = "Sobresaliente";
+        }
+        document.getElementById("notaTexto").textContent = notaTexto;
+    }
 
-function validaLista(indice){
-  if( indice == null || indice == 0 ) {
-    return false;
-  } else return true;
-}
+    function validarValoracion() {
+        var valoracion = document.getElementById("valoracion").value;
+        var valoracionTexto = "";
+        for (var i = 0; i < valoracion; i++) {
+            valoracionTexto += "★";
+        }
+        document.getElementById("valoracionTexto").textContent = valoracionTexto;
+    }
+
+    function validarEdad() {
+        var fechaNacimiento = new Date(document.getElementById("fechaNacimiento").value);
+        var hoy = new Date();
+        var edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+        var m = hoy.getMonth() - fechaNacimiento.getMonth();
+        if (m < 0 || (m === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+            edad--;
+        }
+        document.getElementById("edad").textContent = edad + " años";
+    }
+});
