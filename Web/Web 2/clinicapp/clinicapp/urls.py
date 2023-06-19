7c44c7923b76eddff96aed58a98ff32f8567070d
afvv2602 <44574101+afvv2602@users.noreply.github.com>
2023-06-19 09:25:51 +0200

    Checkpoint

--- Web/Web 2/clinicapp/clinicapp/urls.py
+++ Web/Web 2/clinicapp/clinicapp/urls.py
@@ -4,8 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', doctor_virtual, name='home'),
-        path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='home'),
     path('doctor_virtual/', doctor_virtual, name='doctor_virtual'),
     path('citas/', citas, name='citas'),
 ]
