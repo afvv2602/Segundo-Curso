c3883260dd6621871edd306810b218a58b686675
Adrian Fernandez <afvv26002@gmail.com>
2023-06-19 12:44:47 +0200

    Creacion de los usuarios

--- Web/Web 2/clinicapp/clinic/models.py
+++ Web/Web 2/clinicapp/clinic/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor_profile')
+
+    def __str__(self):
+        return self.user.username
+
+class Paciente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient_profile')
+
+    def __str__(self):
+        return self.user.username
+
 class Perfil(models.Model):
     TIPO_USUARIO_CHOICES = [
         ('PAC', 'Paciente'),
@@ -12,19 +24,12 @@ class Perfil(models.Model):
 class Pregunta(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
-    paciente = models.ForeignKey(User, related_name='preguntas', on_delete=models.CASCADE)
-    medico = models.ForeignKey(User, related_name='respuestas', on_delete=models.SET_NULL, null=True)
+    paciente = models.ForeignKey(Paciente, related_name='preguntas', on_delete=models.CASCADE)
+    medico = models.ForeignKey(Doctor, related_name='respuestas', on_delete=models.SET_NULL, null=True)
     respuesta = models.TextField(blank=True)
 
 class Cita(models.Model):
-    ESPECIALIDAD_CHOICES = [
-        # Aquí debes agregar las especialidades que necesites
-        ('GEN', 'General'),
-        ('PED', 'Pediatria'),
-        # etc.
-    ]
     fecha = models.DateField()
     hora = models.TimeField()
-    especialidad = models.CharField(max_length=3, choices=ESPECIALIDAD_CHOICES)
-    medico = models.ForeignKey(User, related_name='citas_doctor', on_delete=models.CASCADE)
-    paciente = models.ForeignKey(User, related_name='citas_paciente', on_delete=models.CASCADE)
+    medico = models.ForeignKey(Doctor, related_name='citas_doctor', on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, related_name='citas_paciente', on_delete=models.CASCADE)
--- Web/Web 2/clinicapp/clinic/views.py
+++ Web/Web 2/clinicapp/clinic/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import ListView, CreateView
-from .models import Perfil, Pregunta, Cita
+from .models import Doctor, Perfil, Pregunta, Cita
 from django.views import View
 from django.shortcuts import render
 
 class LoginView(View):
     def get(self, request):
-        if request.method == 'POST':
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                pass
         return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('clinic:citas')
+        else:
+            # Aquí puedes manejar el caso de que la autenticación falle, 
+            # por ejemplo, volviendo a renderizar la página de login con un mensaje de error.
+            return render(request, 'login.html', {'error': 'Usuario o contraseña incorrectos'})
 
 class LogoutView(View):
     def get(self, request):
@@ -42,9 +45,11 @@ class CitasView(ListView):
     def get_queryset(self):
         perfil = Perfil.objects.get(user=self.request.user)
         if perfil.tipo_usuario == 'DOC':
-            return Cita.objects.filter(medico=self.request.user) # solo citas del médico
+            doctor = Doctor.objects.get(user=self.request.user)  # Obtén el perfil de Doctor
+            return Cita.objects.filter(medico=doctor)  # Filtro por el perfil de Doctor
         else:
-            return Cita.objects.filter(paciente=self.request.user) # solo citas del paciente
+            return Cita.objects.filter(paciente=self.request.user)
+
         
 
 class ResponderPreguntaView(CreateView):
@@ -80,12 +85,13 @@ class CancelarCitaView(View):
         # Obtenemos la cita por su clave primaria (pk).
         cita = get_object_or_404(Cita, pk=pk)
         # Verificamos que el usuario actual tenga permisos para cancelar la cita.
-        if request.user == cita.usuario:
+        if request.user == cita.paciente:
             # Si el usuario tiene permisos, cancelamos la cita.
             cita.cancelada = True
             cita.save()
-            return redirect('nombre-de-tu-vista-post-cancelacion')
+            return redirect('citas')  # Suponiendo que 'citas' es el nombre de la vista a la que quieres redirigir después de cancelar una cita.
         else:
             # Si el usuario no tiene permisos, devolvemos un mensaje de error.
             return render(request, 'error.html', {'message': 'No tienes permiso para cancelar esta cita.'})
+
 
